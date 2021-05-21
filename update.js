b01a4531c946c359501088b192e1163eb13921bf
cgaedechens <csgaedechens@miuandes.cl>
2021-05-21 16:20:13 -0400

    listo

--- update.js
+++ update.js
@@ -1,23 +1,23 @@
 function update(input,input1,model){
     //const choices = ['Celsius', 'Fahrenheit', 'Kelvin']
     if (input1.lunit == 'Celsius' && input1.runit =='Fahrenheit')
-        convertedvalue = parseInt(input.value)+35
+        convertedvalue = (parseInt(input.value)*9/5) + 32
     else if (input1.lunit == 'Celsius' && input1.runit =='Kelvin')
-        convertedvalue = parseInt(input.value)+35
+        convertedvalue = parseInt(input.value) + 273.15
     else if (input1.lunit == 'Celsius' && input1.runit =='Celsius')
         convertedvalue = parseInt(input.value) //nothing to convert
 
     else if (input1.lunit == 'Fahrenheit' && input1.runit =='Kelvin')
-        convertedvalue = parseInt(input.value)+35
+        convertedvalue = (parseInt(input.value)-32)*5/9 + 273.15 
     else if (input1.lunit == 'Fahrenheit' && input1.runit =='Celsius')
-        convertedvalue = parseInt(input.value)+35
+        convertedvalue =  (parseInt(input.value)-32)*5/9
     else if (input1.lunit == 'Fahrenheit' && input1.runit =='Fahrenheit')
         convertedvalue = parseInt(input.value) //nothing to convert
 
     else if (input1.lunit == 'Kelvin' && input1.runit =='Celsius')
-        convertedvalue = parseInt(input.value)+35
+        convertedvalue = parseInt(input.value) -273.15
     else if (input1.lunit == 'Kelvin' && input1.runit =='Fahrenheit')
-        convertedvalue = parseInt(input.value)+35
+        convertedvalue = (parseInt(input.value)- 273.15)*9/5 + 32
     else if (input1.lunit == 'Kelvin' && input1.runit == 'Kelvin')
         convertedvalue = parseInt(input.value) //nothing to convert
 
